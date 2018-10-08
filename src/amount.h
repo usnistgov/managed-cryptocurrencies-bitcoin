d7df19eb53938f30df6451fa1cb92719becb645d
twoletters <11978675+twoletters@users.noreply.github.com>
2018-10-08 19:56:27 +0000

    Added placeholders for coin transfer mode, role change mode and policy change mode.

--- src/amount.h
+++ src/amount.h
@@ -15,21 +15,51 @@ typedef int64_t CAmountType;
 class CAmount
 {
 private:
-	CAmountType nValue;
+	struct CRoleChangeMode {
+		CAmountType reserved :56;
+		CAmountType action :1;
+		CAmountType a_role :1;
+		CAmountType u_role :1;
+		CAmountType l_role :1;
+		CAmountType c_role :1;
+		CAmountType m_role :1;
+		CAmountType mode   :2;
+	};
+
+	struct CPolicyChangeMode {
+		CAmountType param :32;
+		CAmountType type  :29;
+		CAmountType perm  :1;
+		CAmountType mode  :2;
+	};
+
+	struct CCoinTransferMode {
+		CAmountType amount :63;
+		CAmountType mode   :1;
+	};
+
+	union {
+		struct CRoleChangeMode   rcm;
+		struct CPolicyChangeMode pcm;
+		struct CCoinTransferMode ctm;
+		CAmountType val;
+	} nValue;
 
 public:
 	CAmount() {
-		this->nValue = 0;
+		this->nValue.ctm.mode  = 0;
+		this->nValue.ctm.amount = 0;
 	}
 	CAmount(const CAmountType& nValue) {
-		this->nValue = nValue;
+		this->nValue.ctm.mode  = 0;
+		this->nValue.ctm.amount = nValue;
 	}
 
 	ADD_SERIALIZE_METHODS;
 
 	template <typename Stream, typename Operation>
 	void SerializationOp(Stream& s, Operation ser_action) {
-		READWRITE(nValue);
+		READWRITE(nValue.val);
 	}
 
 	CAmount& operator=(const CAmount& other) {
@@ -38,51 +68,51 @@ public:
 	        return *this;
 	}
 	CAmount& operator=(const CAmountType& other) {
-		this->nValue = other;
+		this->nValue.ctm.amount = other;
 	        return *this;
 	}
 
 	CAmount operator-() const {
-		return CAmount(-(this->nValue));
+		return CAmount(-(this->nValue.ctm.amount));
 	}
 	CAmountType* operator&() {
-		return &this->nValue;
+		return &this->nValue.val;
 	}
 	operator CAmountType() const {
-		return this->nValue;
+		return this->nValue.ctm.amount;
 	}
 
 	CAmount& operator+=(const CAmountType& val) {
-		this->nValue += val;
+		this->nValue.ctm.amount += val;
 		return *this;
 	}
 	CAmount& operator-=(const CAmountType& val) {
-		this->nValue -= val;
+		this->nValue.ctm.amount -= val;
 		return *this;
 	}
 	CAmount& operator*=(const CAmountType& val) {
-		this->nValue *= val;
+		this->nValue.ctm.amount *= val;
 		return *this;
 	}
 	CAmount& operator/=(const CAmountType& val) {
-		this->nValue /= val;
+		this->nValue.ctm.amount /= val;
 		return *this;
 	}
 	CAmount operator/(const CAmountType& val) const {
-		return CAmount(this->nValue / val);
+		return CAmount(this->nValue.ctm.amount / val);
 	}
 	CAmount operator/(const int& val) const {
-		return CAmount(this->nValue / val);
+		return CAmount(this->nValue.ctm.amount / val);
 	}
 	CAmount operator/(const unsigned int& val) const {
-		return CAmount(this->nValue / val);
+		return CAmount(this->nValue.ctm.amount / val);
 	}
 	CAmount& operator>>=(const int& val) {
-		this->nValue >>= val;
+		this->nValue.ctm.amount >>= val;
 		return *this;
 	}
 	CAmount& operator<<=(const int& val) {
-		this->nValue <<= val;
+		this->nValue.ctm.amount <<= val;
 		return *this;
 	}
 };
