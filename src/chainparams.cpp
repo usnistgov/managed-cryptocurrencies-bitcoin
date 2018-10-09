22b76a36b13886f5a70fa21579701263c5405ff6
twoletters <11978675+twoletters@users.noreply.github.com>
2018-10-09 20:07:18 +0000

    Ported new genesis block from mainnet to testnet and regtest.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -226,6 +226,9 @@ public:
         // By default assume that the signatures in ancestors of this block are valid.
         consensus.defaultAssumeValid = uint256S("0x0000000002e9e7b00e1f6dc5123a04aad68dd0f0968d8c7aa45f6640795c37b1"); //1135275
 
+	// Load the default managemet policy for the currency
+	currentPolicy = CManagementPolicy();
+
         pchMessageStart[0] = 0x0b;
         pchMessageStart[1] = 0x11;
         pchMessageStart[2] = 0x09;
@@ -233,10 +236,10 @@ public:
         nDefaultPort = 18333;
         nPruneAfterHeight = 1000;
 
-        genesis = CreateGenesisBlock(1296688602, 414098458, 0x1d00ffff, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1538956802, 3055587597, 0x1d00ffff, 1, currentPolicy.GetGenesisReward());
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"));
-        assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
+        assert(consensus.hashGenesisBlock == uint256S("0x00000000f39e9bff81ddd3d29c06a7007dc84475f310d717d9e6e80f77ff803f"));
+        assert(genesis.hashMerkleRoot == uint256S("0x007d73015f1dde6c6fe96123cb421a9abe38f046962cda780c215eca2981348e"));
 
         vFixedSeeds.clear();
         vSeeds.clear();
@@ -313,6 +316,9 @@ public:
         // By default assume that the signatures in ancestors of this block are valid.
         consensus.defaultAssumeValid = uint256S("0x00");
 
+	// Load the default managemet policy for the currency
+	currentPolicy = CManagementPolicy();
+
         pchMessageStart[0] = 0xfa;
         pchMessageStart[1] = 0xbf;
         pchMessageStart[2] = 0xb5;
@@ -320,10 +326,10 @@ public:
         nDefaultPort = 18444;
         nPruneAfterHeight = 1000;
 
-        genesis = CreateGenesisBlock(1296688602, 2, 0x207fffff, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1538956802, 3055587597, 0x1d00ffff, 1, currentPolicy.GetGenesisReward());
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206"));
-        assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
+        assert(consensus.hashGenesisBlock == uint256S("0x00000000f39e9bff81ddd3d29c06a7007dc84475f310d717d9e6e80f77ff803f"));
+        assert(genesis.hashMerkleRoot == uint256S("0x007d73015f1dde6c6fe96123cb421a9abe38f046962cda780c215eca2981348e"));
 
         vFixedSeeds.clear(); //!< Regtest mode doesn't have any fixed seeds.
         vSeeds.clear();      //!< Regtest mode doesn't have any DNS seeds.
