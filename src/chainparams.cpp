7dd06497eaf67e594b5a46838eb0bbd2c400f544
twoletters <11978675+twoletters@users.noreply.github.com>
2018-11-06 21:44:33 +0000

    Added logic to output genesis block mining information to pair with the new block miner.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -120,10 +120,22 @@ public:
         nDefaultPort = 8333;
         nPruneAfterHeight = 100000;
 
+        uint256 hashGenesisBlock = uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f");
+        uint256 hashMerkleRoot   = uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b");
+
         genesis = CreateGenesisBlock(1231006505, 2083236893, 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"));
-        assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
+        if (consensus.hashGenesisBlock != hashGenesisBlock || genesis.hashMerkleRoot != hashMerkleRoot || true) { // FIXME
+            fprintf(stderr, "Mainnet genesis block needs to be re-mined:\n");
+            fprintf(stderr, "\tVersion...............: %d\n", genesis.nVersion);
+            fprintf(stderr, "\tPrevious block hash...: %s\n", genesis.hashPrevBlock.ToString().c_str());
+            fprintf(stderr, "\tMerkle root...........: %s\n", genesis.hashMerkleRoot.ToString().c_str());
+            fprintf(stderr, "\tTimestamp.............: %u\n", genesis.nTime);
+            fprintf(stderr, "\tDifficulty target.....: %u\n", genesis.nBits);
+            fflush(stderr);
+        }
+        assert(consensus.hashGenesisBlock == hashGenesisBlock);
+        assert(genesis.hashMerkleRoot == hashMerkleRoot);
 
         // Note that of those which support the service bits prefix, most only support a subset of
         // possible options.
@@ -226,10 +238,22 @@ public:
         nDefaultPort = 18333;
         nPruneAfterHeight = 1000;
 
+        uint256 hashGenesisBlock = uint256S("0x000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943");
+        uint256 hashMerkleRoot   = uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b");
+
         genesis = CreateGenesisBlock(1296688602, 414098458, 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"));
-        assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
+        if (consensus.hashGenesisBlock != hashGenesisBlock || genesis.hashMerkleRoot != hashMerkleRoot || true) { // FIXME
+            fprintf(stderr, "Testnet genesis block needs to be re-mined:\n");
+            fprintf(stderr, "\tVersion...............: %d\n", genesis.nVersion);
+            fprintf(stderr, "\tPrevious block hash...: %s\n", genesis.hashPrevBlock.ToString().c_str());
+            fprintf(stderr, "\tMerkle root...........: %s\n", genesis.hashMerkleRoot.ToString().c_str());
+            fprintf(stderr, "\tTimestamp.............: %u\n", genesis.nTime);
+            fprintf(stderr, "\tDifficulty target.....: %u\n", genesis.nBits);
+            fflush(stderr);
+        }
+        assert(consensus.hashGenesisBlock == hashGenesisBlock);
+        assert(genesis.hashMerkleRoot == hashMerkleRoot);
 
         vFixedSeeds.clear();
         vSeeds.clear();
@@ -313,10 +337,23 @@ public:
         nDefaultPort = 18444;
         nPruneAfterHeight = 1000;
 
+        uint256 hashGenesisBlock = uint256S("0x0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206");
+        uint256 hashMerkleRoot   = uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b");
+
         genesis = CreateGenesisBlock(1296688602, 2, 0x207fffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206"));
-        assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
+        if (consensus.hashGenesisBlock != hashGenesisBlock || genesis.hashMerkleRoot != hashMerkleRoot || true) { // FIXME
+            fprintf(stderr, "Regtest genesis block needs to be re-mined:\n");
+            fprintf(stderr, "\tVersion...............: %d\n", genesis.nVersion);
+            fprintf(stderr, "\tPrevious block hash...: %s\n", genesis.hashPrevBlock.ToString().c_str());
+            fprintf(stderr, "\tMerkle root...........: %s\n", genesis.hashMerkleRoot.ToString().c_str());
+            fprintf(stderr, "\tTimestamp.............: %u\n", genesis.nTime);
+            fprintf(stderr, "\tDifficulty target.....: %u\n", genesis.nBits);
+            fprintf(stderr, "%s\n", consensus.hashGenesisBlock.ToString().c_str());
+            fflush(stderr);
+        }
+        assert(consensus.hashGenesisBlock == hashGenesisBlock);
+        assert(genesis.hashMerkleRoot == hashMerkleRoot);
 
         vFixedSeeds.clear(); //!< Regtest mode doesn't have any fixed seeds.
         vSeeds.clear();      //!< Regtest mode doesn't have any DNS seeds.
